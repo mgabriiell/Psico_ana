25b6e473dad7789d0e8d8d59e9ee29c6db422390
Marcos <marcosgabriel898@gmail.com>
2025-11-10 19:11:05 -0300

    app

--- src/views/proprietario/RelatorioFinanceiro.jsx
+++ src/views/proprietario/RelatorioFinanceiro.jsx
@@ -3,7 +3,7 @@ import {
     BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
     ResponsiveContainer, PieChart, Pie, Cell, LineChart, Line
 } from 'recharts';
-import '../../assets/Relatoriofinanceiro.css'; 
+import '../../assets/RelatorioFinanceiro.css'; 
 
 const formatarMoeda = (valor) =>
     new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(valor || 0);
